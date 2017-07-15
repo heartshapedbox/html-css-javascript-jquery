3898137ab234db78571c621df8bc1051faa7829a
Dmitry <babenko.dmitry.mail@gmail.com>
2017-07-15 21:17:59 +0300

    Update script.js

--- javascript/this,bind,call,apply/bind/currying/script.js
+++ javascript/this,bind,call,apply/bind/currying/script.js
@@ -17,7 +17,7 @@ greeting("Female", 19, "Ashley", "Turner"); // Hey, Ashley Turner!
 greeting("Female", 56, "Barbara", "Star"); // Hello, Ms. Barbar Star!
 
 
-var newGreeting = greeting.bind(null, "male", 12);
+var newGreeting = greeting.bind(null, "male", 44);
 
 
 newGreeting("Jeremy", "Stone"); // Hello, Mr. Jeremy Stone!
