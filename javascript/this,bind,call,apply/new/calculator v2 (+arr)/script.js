ba3a769a1fcdf01a3191da6909f8eb1e0fcf8bdd
Dima <babenko.dmitry.mail@gmail.com>
2017-07-27 19:02:58 +0300

    Update script.js

--- javascript/this,bind,call,apply/new/calculator v2 (+arr)/script.js
+++ javascript/this,bind,call,apply/new/calculator v2 (+arr)/script.js
@@ -29,4 +29,4 @@ alert("The result of addition: " + calculator.calculate("2 + 3")); // The result
 alert("The result of subtraction: " + calculator.calculate("2 - 3")); // The result of subtraction: -1
 alert("The result of division: " + calculator.calculate("2 / 3")); // The result of division: 0.6666
 alert("The result of multiplication: " + calculator.calculate("2 * 3")); // The result of multiplication: 6
-alert("The result of cube: " + calculator.calculate("2 *** 3")); // The result of cube: 8
+alert("The result of exponentiation: " + calculator.calculate("2 *** 3")); // The result of exponentiation: 8
