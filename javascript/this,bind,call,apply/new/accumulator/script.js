af4f20bf6a69460f36b745b5fac40bde8a16f570
Dima <babenko.dmitry.mail@gmail.com>
2017-07-28 12:42:08 +0300

    Update script.js

--- javascript/this,bind,call,apply/new/accumulator/script.js
+++ javascript/this,bind,call,apply/new/accumulator/script.js
@@ -13,6 +13,6 @@ var accumulator = new Accumulator(0);
 accumulator.read(); // 2
 accumulator.read(); // 3
 accumulator.read(); // 4
-accumulator.read();  // 5
+accumulator.read(); // 5
 
 alert("The result of operation: " + accumulator.value); // The result of operation: 14
