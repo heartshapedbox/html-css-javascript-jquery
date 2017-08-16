d378ed9bbddadc7652f2f375a4a8510363a39f8b
Dima <babenko.dmitry.mail@gmail.com>
2017-08-16 18:33:05 +0300

    Add files via upload

---
+++ javascript/this,bind,call,apply/decorator pattern/caching decorator v2/script.js
@@ -0,0 +1,44 @@
+// caching decorator
+// make the decorator makeCaching(fn), that takes function fn() and returns wrapper, that caching its result
+// function fn() may have any number of arguments
+
+// sum function for any number of arguments
+function sum() {
+  var count = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    count += arguments[i];
+  }
+  return count;
+}
+
+// decorator
+function makeCaching(sum) {
+  var res = {};
+  return function(x) {
+    x = [].slice.call(arguments);
+    x = x.reduce(function(a, b) {
+      return a + b;
+    });
+
+    if(!(x in res)) {
+      res[x] = sum.call(this, x);
+    }
+    return res[x];
+  }
+}
+
+// wrap function sum() in the decorator
+sum = makeCaching(sum);
+
+var a, b;
+
+a = sum(1, 2, 5);
+b = sum(1, 2, 5);
+console.log(a); // 8
+console.log(b); // 8
+console.log(a == b); // true
+
+
+b = sum(1, 2, 3);
+console.log(b); // 6
+console.log(a == b); // false
