c78de591b5603a0234e220d977db416b259947d0
Dima <babenko.dmitry.mail@gmail.com>
2017-09-06 12:00:45 +0300

    Add files via upload

---
+++ javascript/data types,data structures/objects(for in)/multiply values v2/script.js
@@ -0,0 +1,26 @@
+// write a function multiplyNumeric(menu) that takes the object "menu" and multiply all its key values on 2.
+
+var menu = {
+  width: 100,
+  height: 100,
+  title: "menu"
+};
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+  // isNaN (checks whether a value is a number)
+  // isFinite (checks whether a value is a special number: NaN/Infinity/-Infinit)
+}
+
+function multiplyNumeric(menu) {
+  for (var key in menu) {
+    if (isNumeric(menu[key])) {
+      menu[key] *= 2;
+    }
+  }
+  return menu;
+}
+
+console.log(multiplyNumeric(menu));
+
+// { width: 200, height: 200, title: 'menu' }
