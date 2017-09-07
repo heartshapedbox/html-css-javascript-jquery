ce14f5ba8c040160f82af9c4ce752314351a585a
Dima <babenko.dmitry.mail@gmail.com>
2017-09-07 16:02:09 +0300

    Update script.js

--- javascript/data types,data structures/strings/length of identical characters/script.js
+++ javascript/data types,data structures/strings/length of identical characters/script.js
@@ -9,7 +9,7 @@ function getLengthOfChar() {
   var newArr = [];
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === char) {
-      newArr += arr[i];
+      newArr.push(arr[i]);
     }
   }
   return newArr.length;
