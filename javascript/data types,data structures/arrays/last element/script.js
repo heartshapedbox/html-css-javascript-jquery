6436995726f214b16e3537f128c9a4b4ac5ce5bc
Dima <babenko.dmitry.mail@gmail.com>
2017-09-08 17:46:10 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/last element/script.js
@@ -0,0 +1,5 @@
+// get the last element form the array "goods"
+
+var goods = ["Fruits", "Vegetables", "Chocolate", "Coffee", "Tea", "Meat", "Fish", "Drinks", "Clothing"];
+
+console.log(goods[goods.length - 1]); // Clothing
