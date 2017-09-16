0c11a2736ebb6cb7384c12c636f2388c6c4dc012
Dima <babenko.dmitry.mail@gmail.com>
2017-09-16 20:39:53 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/subarray of the largest sum/script.js
@@ -0,0 +1,20 @@
+// Write the function that finds the sum of the elements of the uninterrupted subarray
+
+
+arr = [10, -2, 2, 3, -9, 2];
+
+function getMaxSubSum (arr) {
+  var max = 0;
+  var part = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    part += arr[i];
+    max = Math.max(max, part);
+    if (part < 0) {
+      part = 0;
+    }
+  }
+  return max;
+}
+
+console.log(getMaxSubSum(arr)); // 13
