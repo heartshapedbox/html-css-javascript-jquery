59a0e2bdc4ba901c5547dae90f7969323e7ea98d
Dima <babenko.dmitry.mail@gmail.com>
2017-09-21 18:42:18 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/reverse/script.js
@@ -0,0 +1,6 @@
+// Change the order of the elements from the array "arr" to reverse
+
+var arr = ["element1", "element2", "element3", "element4"];
+arr.reverse();
+
+console.log(arr); // [ 'element4', 'element3', 'element2', 'element1' ]
