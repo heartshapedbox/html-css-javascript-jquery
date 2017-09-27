734191a784e00a4cb30ee9993d6de531a1625a79
Dima <babenko.dmitry.mail@gmail.com>
2017-09-27 17:56:57 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/copy and sort the array v2/script.js
@@ -0,0 +1,12 @@
+// Make sorted array "arrSorted" from the array "arr". Original array should not be changed.
+
+var arr = ["element2", "element4", "element5", "element1", "element3"];
+var arrSorted = [];
+
+for (var i = 0; i < arr.length; i++) {
+  arrSorted.push(arr[i]);
+  arrSorted.sort();
+}
+
+console.log(arr); // [ 'element2', 'element4', 'element5', 'element1', 'element3' ]
+console.log(arrSorted); // [ 'element1', 'element2', 'element3', 'element4', 'element5' ]
