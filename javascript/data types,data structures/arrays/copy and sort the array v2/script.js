2772c522f71f881b7cf87b3af0d6461494a65b33
Dima <babenko.dmitry.mail@gmail.com>
2017-09-27 20:23:30 +0300

    Update script.js

--- javascript/data types,data structures/arrays/copy and sort the array v2/script.js
+++ javascript/data types,data structures/arrays/copy and sort the array v2/script.js
@@ -5,8 +5,9 @@ var arrSorted = [];
 
 for (var i = 0; i < arr.length; i++) {
   arrSorted.push(arr[i]);
-  arrSorted.sort();
 }
+
+arrSorted.sort();
 
 console.log(arr); // [ 'element2', 'element4', 'element5', 'element1', 'element3' ]
 console.log(arrSorted); // [ 'element1', 'element2', 'element3', 'element4', 'element5' ]
