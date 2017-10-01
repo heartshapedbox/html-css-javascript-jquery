cc9580036a75aebb66c48c715f981f4f99c133c1
Dima <babenko.dmitry.mail@gmail.com>
2017-10-01 20:49:44 +0300

    Update script.js

--- javascript/data types,data structures/arrays/linked list/script.js
+++ javascript/data types,data structures/arrays/linked list/script.js
@@ -14,16 +14,18 @@ var list = {
   }
 };
 
-var arr = [];
-
 function printList(list) {
   while(list) {
-    arr.push(list.value);
+    console.log(list.value);
     list = list.next;
   }
 }
 
 printList(list);
 
-
-console.log(arr); // [ 1, 2, 3, 4 ]
+/*
+1
+2
+3
+4
+*/
