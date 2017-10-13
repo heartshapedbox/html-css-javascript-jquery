e4d4c5e307460e5e68caa88aed2dfbe13793893a
Dima <babenko.dmitry.mail@gmail.com>
2017-10-13 09:26:24 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/forEach/script.js
@@ -0,0 +1,15 @@
+// Method "forEach" use for going through an array
+
+var arr = ["Water", "Food", "Clothes", "Vehicle", "Building"];
+
+arr.forEach(function(item, i, arr) {
+  console.log(i + ": " + item + " (from the array: " + arr + ")");
+});
+
+/*
+0: Water (from the array: Water,Food,Clothes,Vehicle,Building)
+1: Food (from the array: Water,Food,Clothes,Vehicle,Building)
+2: Clothes (from the array: Water,Food,Clothes,Vehicle,Building)
+3: Vehicle (from the array: Water,Food,Clothes,Vehicle,Building)
+4: Building (from the array: Water,Food,Clothes,Vehicle,Building)
+*/
