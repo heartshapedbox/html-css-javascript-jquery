279cd82a2057e3956d900afd338b431c8216a697
Dima <babenko.dmitry.mail@gmail.com>
2017-10-17 09:34:54 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/filter/script.js
@@ -0,0 +1,9 @@
+// Method arr.filter is used for the filtration of the array
+
+var arr = ["element1", "element2", 1, 2, 3];
+
+arr = arr.filter(function(arg) {
+  return typeof arg == "number";
+});
+
+console.log(arr); // [ 1, 2, 3 ]
