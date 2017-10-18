afc2c655e68d36da1e7d4ddb57b1af5c274c0435
Dima <babenko.dmitry.mail@gmail.com>
2017-10-18 10:10:24 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/reduce/script.js
@@ -0,0 +1,8 @@
+// Concatenate the arrays
+
+var numArr = [1,2,3,4,5];
+var strArr = ["element1", "element2", "element3", "element4"];
+
+var newArr = numArr.concat(strArr);
+
+console.log(newArr); // [ 1, 2, 3, 4, 5, 'element1', 'element2', 'element3', 'element4' ]
