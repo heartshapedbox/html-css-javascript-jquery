6154e31a49f5878f7aa266d5b219f45e78306395
Dima <babenko.dmitry.mail@gmail.com>
2017-10-20 10:05:46 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/length of each element (loop) v2/script.js
@@ -0,0 +1,12 @@
+// Get the length of each element from the array "arr" and push it into a new array "arrLength"
+// Use a method "map"
+
+var arr = ["Food", "Clothes", "Vehicle", "Buildings", "Water"];
+
+var arrLength = [];
+
+for (var i = 0; i < arr.length; i++) {
+  arrLength.push(arr[i].length);
+}
+
+console.log(arrLength); // [ 4, 7, 7, 9, 5 ]
