780ca39935eb0a717e00d974dbea2c46c28eb82b
Dima <babenko.dmitry.mail@gmail.com>
2017-10-26 09:55:40 +0300

    Add files via upload

---
+++ javascript/data types,data structures/arrays/check undefined argument/script.js
@@ -0,0 +1,17 @@
+// Check if the function f(x) has a "undefined" argument
+// if there is a first argument - let it be 1
+// if there is no first argument - let it be 0
+
+function f(x) {
+  for (var i = 0; i < arguments.length; i++) {
+    x = 1;
+  }
+
+  if (x === undefined)  x = 0;
+  console.log(x);
+}
+
+
+f(undefined); // 1
+f(); // 0
+f("a"); // 1
