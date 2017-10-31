ad607be356357bbbd1096e2099289ce1a754e343
Dima <babenko.dmitry.mail@gmail.com>
2017-10-31 22:54:51 +0200

    Add files via upload

---
+++ javascript/data types,data structures/date & time/set date components/script.js
@@ -0,0 +1,21 @@
+// Set date components
+
+var today = new Date;
+
+today.setFullYear(2002);
+alert(today); // Wed Oct 30 2002 10:17:34
+
+today.setMonth(2);
+alert(today); // Sat Mar 30 2002 10:17:34
+
+today.setDate(25);
+alert(today); // Mon Mar 35 2002 10:17:34
+
+today.setHours(5);
+alert(today); // Mon Mar 35 2002 05:17:34
+
+today.setMinutes(27);
+alert(today); // Mon Mar 35 2002 05:27:34
+
+today.setSeconds(50);
+alert(today); // Mon Mar 35 2002 05:27:50
