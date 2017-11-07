0800280e45df550b7fb98f45ec3e5372556ab03b
Dima <babenko.dmitry.mail@gmail.com>
2017-11-07 10:16:38 +0200

    Add files via upload

---
+++ javascript/data types,data structures/date & time/name of the week day v2/script.js
@@ -0,0 +1,10 @@
+// Make a function getWeekDay(date) that returns a week day in the short format "Mon", "Thue", "Wed" etc.
+
+var date = new Date(2012, 0, 3);
+var arr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+function getWeekDay(date) {
+  return arr[date.getDay()];
+}
+
+console.log(getWeekDay(date)); // Tue
