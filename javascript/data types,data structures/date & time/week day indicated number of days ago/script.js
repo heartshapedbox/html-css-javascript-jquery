fdc94e64a36fa342c1b8218e17dc6121626207d2
Dima <babenko.dmitry.mail@gmail.com>
2017-11-11 21:54:39 +0200

    Add files via upload

---
+++ javascript/data types,data structures/date & time/week day indicated number of days ago/script.js
@@ -0,0 +1,16 @@
+// Create a function getDayAgo(date, days) that returns the week day that was "days" days ago
+// Function should not change the object "date"
+
+var date = new Date(2017,3,13);
+var options = {
+  weekday: "long"
+}
+
+function getWeekDayAgo(date, days) {
+  var dateCopy = new Date(date);
+  var day = dateCopy.setDate(dateCopy.getDate() - days);
+  return dateCopy.toLocaleString("en-US", options);
+}
+
+alert(date.toLocaleString("en-US", options)); // Thursday
+alert(getWeekDayAgo(date, 2)); // Tuesday
