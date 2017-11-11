ec7f5d48399a52d7c12938ab54028b7385776483
Dima <babenko.dmitry.mail@gmail.com>
2017-11-11 21:53:30 +0200

    Add files via upload

---
+++ javascript/data types,data structures/date & time/date indicated number of days ago/script.js
@@ -0,0 +1,17 @@
+// Create a function getDateAgo(date, days) that returns the date that was "days" days ago
+// Function should not change the object "date"
+
+var date = new Date(2017,3,13);
+
+function getDateAgo(date, days) {
+  var dateCopy = new Date(date);
+
+  var year = dateCopy.getFullYear();
+  var month = dateCopy.getMonth();
+  var day = dateCopy.getDate() - days;
+
+  return dateCopy = new Date(year,month,day);
+}
+
+alert(date); // Thu Apr 13 2017 00:00:00 GMT+0300 (EEST)
+alert(getDateAgo(date, 2)); // Tue Apr 11 2017 00:00:00 GMT+0300 (EEST)
